23b3d72955e016c42c507ff785218db0563cd0ea
sporritt <simon.porritt@gmail.com>
2015-06-29 22:39:42 +1000

    add commitOnBlur support

--- src/schmedit.js
+++ src/schmedit.js
@@ -4,6 +4,7 @@
         CONTENT_EDITABLE = "contenteditable",
         KEYPRESS = "keydown",
         CLICK = "click",
+        BLUR = "blur",
         classes = { "editing":"schmediting", "editable":"schmeditable" },
         _getClass = function(ref, claz) {
             var c = [ ref ? classes[ref] || "" : "" ];
@@ -23,10 +24,12 @@
                 }
             };
             var _edit = function() {
+                if (options.onEdit && options.onEdit(this) === false) return;
                 if (_editing) _close();
                 _current = this;
                 mottle.on(document, CLICK, _maybeCancel);
                 mottle.on(_current, KEYPRESS, _keypress);
+                mottle.on(_current, BLUR, _maybeCancel);
                 this._schmeditCN = this.className;
                 this._schmeditValue = this.innerHTML;
                 this.className = [ this.className, editingClass ].join(" ");
@@ -39,6 +42,7 @@
                 _current.removeAttribute(CONTENT_EDITABLE);
                 mottle.off(document, CLICK, _maybeCancel);
                 mottle.off(_current, KEYPRESS, _keypress);
+                mottle.off(_current, BLUR, _maybeCancel);
                 _editing = false;
                 if (andCommit && options.onCommit) {
                     options.onCommit(_current, _current.innerHTML);
@@ -48,7 +52,8 @@
                 }
             };
             var _maybeCancel = function(e) {
-                if (e.target.className.indexOf("schmediting") == -1) _close();
+                if (_editing && (e.target.className == null || e.target.className.indexOf("schmediting") == -1 || e.type === "blur"))
+                    _close(options.commitOnBlur === true);
             };
             var _editing = false, _replacedElement = null;
             var bindArgs = arguments.length == 2 ? [ arguments[0], CLICK ] : [arguments[0], CLICK, arguments[1]];
